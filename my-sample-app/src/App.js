f64559593c19d5c5d4c73d3b38b65d53c1d4116f
Ajith <ajith@gmail.com>
2025-06-18 13:08:27 +0530

    DevOps Sample Application

---
+++ my-sample-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        className="devops-icon"
+        src="https://cdn-icons-png.flaticon.com/512/5969/5969059.png"
+        alt="DevOps Icon"
+      />
+      <h1>Welcome to DevOps</h1>
+    </div>
+  );
+}
+
+export default App;
+
